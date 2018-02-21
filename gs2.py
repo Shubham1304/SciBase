01177cd058c6608a03ada24d3391d1b927aeba0c
Shubham <shubhams3013@gmail.com>
2018-02-22 00:41:26 +0530

    The one with the broken json data

--- gs2.py
+++ gs2.py
@@ -21,29 +21,56 @@ button.click()
 element = WebDriverWait(browser,30).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'#gsc_a_b > tr:nth-child(1) > td.gsc_a_t > a')))
 source1=browser.page_source
 soup = BeautifulSoup(source1,'html.parser')
+raw_name = soup.find("td",{"class":"gsc_a_t"})
+name=raw_name.find("a",{"class":"gsc_a_at"})
+#print(name.text)
+str1=[]
+str1.append("{\""+name.text+"\":{")
+
+
 button2 = browser.find_element_by_css_selector('tr.gsc_a_tr:nth-child(1) > td:nth-child(2) > a:nth-child(1)')
 button2.click()
 source2=browser.page_source
 soup = BeautifulSoup(source2,'html.parser')
 # Path to be created
-
 path = "/home/shubham/directory1"
 os.makedirs(path, exist_ok=True)
 print ("Path is created")
-str1=[]
-str1.append('{\"citations\":[')
 file=open('/home/shubham/directory1/abcd.json',"a")
-str2=[]
-ele=[]
+for i in range(3,5):
+    s1='#gs_n > center:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child('
+    s3=') > a:nth-child(1)'
+    #s2=str(i)
+    button3=browser.find_element_by_css_selector(s1+str(i)+s3)
+    button3.click()                             #gs_n > center:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(4) > a:nth-child(1)
+    source3=browser.page_source                 #gs_n > center:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(5) > a:nth-child(1)
+    soup=BeautifulSoup(source3,'html.parser')
+#str2=[]
+#ele=[]
 #for i in range(0,5):
-for ele in soup.find_all("div",{"class":"gs_ri"}):  #for the whole class
-    for ele1 in ele.find_all("h3",{'class':"gs_rt"}):
-        for a1 in ele1.find_all('a'):
-            str1.append(a1.text)
-    for ele2 in ele.find_all("div",{'class':'gs_a'}):
-        for a2 in ele2.find_all('a'):
-            str1.append(a2.text)
-str1.append(']}')
-var = ' '.join(str1)
-file.write(var)
+    for ele in soup.find_all("div",{"class":"gs_ri"}):  #for the whole class
+        for ele1 in ele.find_all("h3",{'class':"gs_rt"}):  #for the name of paper
+            for a1 in ele1.find_all('a'):
+                redditAll1 = ele1.find_all("a")
+                print(len(redditAll1))
+                leng1=len(redditAll1)
+                str1.append("\""+a1.text+"\":[{")
+                leng1-=1
+        for ele2 in ele.find_all("div",{'class':'gs_a'}):   #for the names of autors
+            for a2 in ele2.find_all('a'):
+                redditAll2 = ele2.find_all("a")
+                print(len(redditAll2))
+                leng2=len(redditAll2)
+                str1.append("\"Author\":\"" + a2.text + "\"}")
+                leng2-=1
+                if leng2 != 0:
+                    str1.append(",")
+                else:
+                    str1.append("]")
+                    if leng1 !=0:
+                        str1.append(",")
+
+    str1.append("}}")
+    var = ' '.join(str1)
+    file.write(var)
 file.close()
